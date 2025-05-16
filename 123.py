deccb5af32e32993f8c52798d2cb4c1a7abe4210
Hieu2k1kakaka <103618896+Hieu2k1kakaka@users.noreply.github.com>
2025-05-16 14:57:54 +0700

    Update 123.py

--- 123.py
+++ 123.py
@@ -24,7 +24,6 @@ st.markdown(
     <style>
     [data-testid="stAppViewContainer"] {{
         background-image: url("data:image/png;base64,{image_base64}");
-        background-size: cover;
         background-position: center;
         background-repeat: no-repeat;
     }}
